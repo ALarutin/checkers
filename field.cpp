dfdce5a1f8f3fa7efb2f701e9c510f52de8ca15a
mac <a.larutin@yandex.ru>
2019-05-14 14:04:19 +0300

    a bit of makeup

--- field.cpp
+++ field.cpp
@@ -12,10 +12,10 @@ FieldGraph::FieldGraph(const uint _horizontalSize, const uint _verticalSize) :
 //- обходим линию за линией, храним только данный предыдущей и нынешней линии;
 //- сохранием в "клетке"(cell) колличество ходов до этой клетке.
 uint FieldGraph::StepsNumber(uint coordinate) {
+
     if (horizontalSize == 1 || verticalSize == 1) {
         return 0;
     }
-    uint steps = 0;
 
     cell start{coordinate - 1, 1};
 
@@ -25,16 +25,20 @@ uint FieldGraph::StepsNumber(uint coordinate) {
     for (uint i = 1; i < verticalSize; i++) {
         list<cell> thisLine;
         while (!lastLine.empty()) {
+
             cell last = lastLine.front();
             lastLine.pop_front();
+
             cell nextLeft{last.coordinate - 1};
             if (!thisLine.empty()) {                                //Проверяем, если список не пуст,
                 nextLeft = thisLine.back();                         //то у этой клетки больше 1 родителя,
                 thisLine.pop_back();
             }
             nextLeft.paths += last.paths;                           //и нужно прибавить новое количетсво путей.
+
             cell nextRight{last.coordinate + 1};
             nextRight.paths += last.paths;
+
             if (last.coordinate == 0) {
                 thisLine.push_back(nextRight);
             } else if (last.coordinate == horizontalSize - 1) {
@@ -46,6 +50,8 @@ uint FieldGraph::StepsNumber(uint coordinate) {
         }
         lastLine = thisLine;
     }
+
+    uint steps = 0;
     while (!lastLine.empty()) {                                     //Обходим полученный в конце список, суммируя все пути.
         cell last = lastLine.front();
         lastLine.pop_front();
